b3f8c6f430b8d1a4d31b4ad433bd88b28ded848e
monica.puisoru <elsene.scarlett@yahoo.com>
2021-03-25 10:08:23 +0200

    added test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +13,10 @@ class DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void testAddMethod (){
+		Calculator calculator = new Calculator();
+		int sum = calculator.add(2, 3);
+		Assert.isTrue(sum==5, "Test failed, values differ");
+	}
 }

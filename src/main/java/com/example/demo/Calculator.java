d67bd01ac8d51ad8dad10946ab08f7bcac2d9b87
monica.puisoru <elsene.scarlett@yahoo.com>
2021-03-25 09:25:48 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Calculator.java
@@ -0,0 +1,6 @@
+package com.example.demo;
+
+public class Calculator {
+
+    public int add (int a, int b){return a+b; }
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DemoApplication.class, args);
+		Calculator c = new Calculator();
+		System.out.println(c.add(2, 3));
+	}
+
+}
